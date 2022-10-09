12ef181b6e4fa66c4799b27af0834f1dfc4fb78e
SemperEtAnte <zhorik2800999@gmail.com>
2022-10-09 16:48:23 +0300

    Комментарии в конфигах и миграциях

--- src/main/resources/db/migration/V1__users.sql
+++ src/main/resources/db/migration/V1__users.sql
@@ -1,21 +1,27 @@
+/* Создаем таблицу пользователей */
 CREATE TABLE IF NOT EXISTS learn_users
 (
-    id              SERIAL8 PRIMARY KEY,
-    login           VARCHAR(16) NOT NULL,
-    email           TEXT NOT NULL,
-    password_digest TEXT NOT NULL,
-    role            TEXT NOT NULL DEFAULT 'USER',
-    registered_at   TIMESTAMP     DEFAULT CURRENT_TIMESTAMP
+    id              SERIAL8 PRIMARY KEY, /* Первичный ключ ID serial8 == bigint auto_increment */
+    login           VARCHAR(16) NOT NULL, /* Логин, не более 16 символов */
+    email           TEXT NOT NULL, /* Почта */
+    password_digest TEXT NOT NULL, /* Хэш пароля */
+    role            TEXT NOT NULL DEFAULT 'USER', /* Роль в системе. По-умолчанию юзер */
+    registered_at   TIMESTAMP     DEFAULT CURRENT_TIMESTAMP /* Дата регистрации */
 );
+/* Уникальный индекс на логин, без учета регистра */
 CREATE UNIQUE INDEX IF NOT EXISTS learn_users_login_idx ON learn_users (LOWER(login));
+/* Уникальный индекс на почту, без учета регистра */
 CREATE UNIQUE INDEX IF NOT EXISTS learn_users_email_idx ON learn_users (LOWER(email));
 
+/* Таблица с токенами для перевыпуска JWT */
 CREATE TABLE IF NOT EXISTS learn_refresh_tokens
 (
-    token      TEXT PRIMARY KEY,
-    user_id    int8 NOT NULL REFERENCES learn_users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    expires_at TIMESTAMP NOT NULL
+    token      TEXT PRIMARY KEY, /* Сам токен перевыпуска идет как первичный ключ (нет смысла делать ИД, т.к. зависимостей нет) */
+    user_id    int8 NOT NULL REFERENCES learn_users (id) ON DELETE CASCADE ON UPDATE CASCADE, /* Пользователь к которому подвязывается токен*/
+    expires_at TIMESTAMP NOT NULL /* Время истечения жизни токена*/
 );
 
+/* Индекс на зависимое поле для ускорения JOIN запросов */
 CREATE INDEX IF NOT EXISTS learn_reset_tokens_user_idx ON learn_refresh_tokens (user_id);
+/* Индекс на поле истечения для запросов удаления истекших */
 CREATE INDEX IF NOT EXISTS learn_reset_tokens_expires_idx ON learn_refresh_tokens (expires_at);
--- src/main/resources/db/migration/V2__messages.sql
+++ src/main/resources/db/migration/V2__messages.sql
@@ -1,9 +1,13 @@
+/* Таблица сообщений */
 CREATE TABLE IF NOT EXISTS learn_user_messages
 (
-    id        SERIAL8 PRIMARY KEY,
-    author_id int8 NOT NULL REFERENCES learn_users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    message   TEXT NOT NULL,
-    sent_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id        SERIAL8 PRIMARY KEY, /* ИД сообщений первичный ключ*/
+    author_id int8 NOT NULL REFERENCES learn_users (id) ON DELETE CASCADE ON UPDATE CASCADE, /* Подвязка к автору сообщения*/
+    message   TEXT NOT NULL, /* Текст сообщения */
+    sent_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP /* Дата отправки сообщения */
 );
+/* Индекс на пользователя для ускорения запросов JOIN */
 CREATE INDEX IF NOT EXISTS learn_user_messages_author_idx ON learn_user_messages (author_id);
+
+/* Индекс на время отправки для сортировки. DESC - означает что индексы будут упорядочиваться снизу-вверх*/
 CREATE INDEX IF NOT EXISTS learn_user_messages_sent_idx ON learn_user_messages (sent_at DESC);
